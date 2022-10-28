93f875c572fa873b43f47109e20e8d3db25f3e4e
Paradox <parax_85@mail.ru>
2022-10-28 16:22:04 +0300

    init commit; added analytics wrapper base

---
+++ Analytics/AnalyticsWrapperBase.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class AnalyticsWrapperBase : MonoBehaviour
+{
+    static List<AnalyticsWrapperBase> wrappers = new List<AnalyticsWrapperBase>();
+
+    protected static string userId;
+
+    protected virtual void Awake()
+    {
+        wrappers.Add(this);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        wrappers.Remove(this);
+    }
+
+    protected virtual void onLogin()
+    {
+
+    }
+
+    protected abstract void sendEvent(string eventType, Dictionary<string,object> data = null);
+    
+    public static void SendEvent(string eventType, Dictionary<string, object> data = null)
+    {
+        foreach (var wr in wrappers)
+            wr.sendEvent(eventType, data);
+    }
+
+    public static void OnLogin(string uid)
+    {
+        userId = uid;
+        foreach (var wr in wrappers)
+            wr.onLogin();
+    }
+}
