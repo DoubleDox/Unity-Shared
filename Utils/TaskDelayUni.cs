7f4b8b7588845ef02a43cac8693793160900f682
DoubleDox <parax85@gmail.com>
2024-06-11 16:42:09 +0300

    added Task.Delay universal implementation for WEBGL support

---
+++ Utils/TaskDelayUni.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public static class TaskDelayUni
+{
+    public static async Task WaitWebgl(float seconds)
+    {
+        float start = Time.time;
+
+        while (Time.time < start + seconds)
+            await Task.Yield();
+    }
+
+    public static async Task WaitUni(float seconds)
+    {
+#if !UNITY_WEBGL
+        await Task.Delay(Mathf.RoundToInt(seconds * 1000));
+#else
+        await WaitWebgl(seconds);
+#endif
+	}
+}
