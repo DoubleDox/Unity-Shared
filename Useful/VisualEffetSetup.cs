0b5d211f45aae5796206804b6bc4d2f50e0efcf0
DoubleDox <parax85@gmail.com>
2025-11-28 15:07:43 +0300

    added IResource; added VisualEffectSetup base class; v1.0.20

---
+++ Useful/VisualEffetSetup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public abstract class VisualEffectSetup : ScriptableObject
+{
+    public abstract void Execute(GameObject initiator, VisualEffectContext context = null);
+}
+
+// for future implementations
+public class VisualEffectContext
+{
+
+}
