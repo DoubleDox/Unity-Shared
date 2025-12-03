e3e00432c968c5439cbb49677319687d4f9eb43f
DoubleDox <parax85@gmail.com>
2025-12-03 03:26:25 +0300

    IResource get ID from gameobject

--- Interfaces/Common.cs
+++ Interfaces/Common.cs
@@ -1,4 +1,13 @@
-﻿public interface IResource
+﻿using UnityEngine;
+
+public interface IResource
 {
     string Id { get; }
+
+    public static string GetId(GameObject go)
+    {
+        if (go != null && go.TryGetComponent<IResource>(out var r))
+            return r.Id;
+        return null;
+    }
 }
