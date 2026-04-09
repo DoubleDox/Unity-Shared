4953dbb6f6a19bd0302c341feed0ab6b020b1d88
DoubleDox <parax85@gmail.com>
2026-04-09 16:28:08 +0300

    new livings declaration (breaking changes!)

--- Interfaces/Livings.cs
+++ Interfaces/Livings.cs
@@ -1,23 +1,61 @@
 using UnityEngine;
 
-public interface ILivingActor
+public interface ILivingActor : IComponentHolder
 {
     bool IsAlive { get; }
 }
 
+public struct LivingImpact
+{
+    /// <summary>
+    /// below zero - to deal damage, above zero - to heal
+    /// </summary>
+    public int hpDiff;
+    /// <summary>
+    /// Damage type (used for vulnerability calc, for example)
+    /// </summary>
+    public string impact;
+    /// <summary>
+    /// armed ammo in offender weapon
+    /// </summary>
+    public IItemElement ammo;
+    public IWeaponHolder offender;
+
+    public bool checkOnly;
+    /// <summary>
+    /// Ignore any damage processors (absorbers, like armor)
+    /// </summary>
+    public bool ignoreProcessors;
+
+    public LivingImpact(int _hpDiff, IWeaponHolder _offender = null, string impactType = null)
+    {
+        ignoreProcessors = false;
+        hpDiff = _hpDiff;
+        impact = impactType;
+        ammo = null;
+        offender = _offender;
+        checkOnly = false;
+    }
+}
+
 public interface ILivingDeathHandler
 {
-    void OnDeath(GameObject weapon, GameObject offender);
+    void OnDeath(LivingImpact livingImpact);
 }
 
 public interface ILivingDamageHandler
 {
-    void OnDamage(ILivingImpact livingImpact, GameObject offender);
+    void OnDamage(LivingImpact livingImpact);
 }
 
-public interface ILivingImpact
+/// <summary>
+/// For items/equipment that absorbs damage (like armor)
+/// </summary>
+public interface ILivingDamageProcessor
 {
-    int HpDiff { get; }
+    void OnDamageProcessor(LivingImpact damage);
+
+    int Amount { get; }
 }
 
 public interface INonDestructableOnDeath
