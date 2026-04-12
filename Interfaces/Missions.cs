bbf3ed20a5bd08532e2b6ce9af79a2ae520327a5
DoubleDox <parax85@gmail.com>
2026-04-12 23:23:08 +0300

    missions interfaces v1.0.29

---
+++ Interfaces/Missions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+public interface IMissionHolder
+{
+    bool IsEnabled { get; }
+
+    int InternalID { get; }
+
+    void StoreProps(Dictionary<string, object> props);
+}
+
+public interface IMissionsPack
+{
+    List<IMissionHolder> Missions { get; }
+}
